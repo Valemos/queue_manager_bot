983208b0ff7c7fb8557b3109d984a82bcc3b57e7
anton_skrypnyk <programworkerbox@gmail.com>
2020-03-30 00:56:34 +0300

    correction

--- runBot.py
+++ runBot.py
@@ -1,8 +1,7 @@
 from HelperBot import QueueBot
 
-if __name__=='__main__':
-    bot_token = "948057504:AAH3Le3JvcPsSW-16M8bUsVZmAICjbqL6Js"
+bot_token = "948057504:AAH3Le3JvcPsSW-16M8bUsVZmAICjbqL6Js"
 
-    my_bot = QueueBot(bot_token)
+my_bot = QueueBot(bot_token)
 
-    my_bot.start()
+my_bot.start()
