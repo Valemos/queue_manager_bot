cb32746ad34c1bb5aa29ec5761e721d8cabe39d4
anton_skrypnyk <programworkerbox@gmail.com>
2020-03-30 00:41:54 +0300

    token added

--- runBot.py
+++ runBot.py
@@ -1,7 +1,8 @@
 from HelperBot import QueueBot
 
-bot_token = "948057504:AAH3Le3JvcPsSW-16M8bUsVZmAICjbqL6Js"
+if __name__=='__main__':
+    bot_token = "948057504:AAH3Le3JvcPsSW-16M8bUsVZmAICjbqL6Js"
 
-my_bot = QueueBot(bot_token)
+    my_bot = QueueBot(bot_token)
 
-my_bot.start()
+    my_bot.start()
